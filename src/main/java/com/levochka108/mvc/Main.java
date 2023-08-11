d04922dd1f0d433b6222b09347918de695569677
Dmitrii Klochkov <d.klochkov9421@gmail.com>
2023-08-11 14:24:29 +0300

    Промежуточная аттестация в GeekBrains. Красно черное дерево на JAVA

--- src/main/java/com/levochka108/mvc/Main.java
+++ src/main/java/com/levochka108/mvc/Main.java
@@ -2,23 +2,26 @@ package com.levochka108.mvc;
 
 
 import com.levochka108.mvc.list.LinkedList;
+import com.levochka108.mvc.redblacktree.ViewRedBlackTree;
 
 public class Main {
+    /*    public static void main(String[] args) {
+            LinkedList linkedList = new LinkedList();
+            linkedList.addNode(1);
+            linkedList.addNode(2);
+            linkedList.addNode(3);
+            linkedList.addNode(4);
+
+            System.out.println("Исходный односвязный список:");
+            linkedList.display();
+
+            linkedList.reverse();
+
+            System.out.println("Односвязный список после разворота:");
+            linkedList.display();
+        }*/
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList();
-        linkedList.addNode(1);
-        linkedList.addNode(2);
-        linkedList.addNode(3);
-        linkedList.addNode(4);
-
-        System.out.println("Исходный односвязный список:");
-        linkedList.display();
-
-        linkedList.reverse();
-
-        System.out.println("Односвязный список после разворота:");
-        linkedList.display();
+        ViewRedBlackTree show = new ViewRedBlackTree();
+        show.run();
     }
-
-
 }
